5fa083767d599e192197554874b277f4747d77cd
opsb <oliver@opsb.co.uk>
2017-02-11 15:56:22 +0100

    Add waypoints to update url and info

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,2 +1,27 @@
 //= require_tree './vendor'
 //= require_tree './modules'
+
+var elements = document.querySelectorAll('[data-name]');
+
+
+for(var i = 0; i < elements.length; i++) {
+  (function(element) {
+    new Waypoint.Inview({
+      element: element,
+      enter: function() {
+        updateFooter(element.dataset.name);
+        updateInfo(element.dataset.info);
+      }
+    });
+  })(elements[i]);
+}
+
+function updateFooter(url) {
+  var title = document.querySelector(".footer .title");
+  document.querySelector(".footer .title a").href = url;
+  document.querySelector(".footer .title h1").innerHTML = url;
+}
+
+function updateInfo(info) {
+  document.querySelector(".info h6").innerHTML = info;
+}
